56d4fff49c3e83ce531a319f8542f1724c596e5f
shreepadjoshi <shreepadjoshi@35691da0-5893-11de-8832-3f4c773c73ae>
2009-09-05 15:31:01 +0000

    Added by Shripad and Chetan

---
+++ trunk/EJB3.0/EJB3SessionBean/ejbModule/com/Advise.java
@@ -0,0 +1,39 @@
+package com;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import pojo.Book;
+
+/**
+ * Session Bean implementation class Advise
+ */
+@Stateless
+public class Advise implements AdviseRemote {
+
+	
+	@PersistenceContext	EntityManager em;
+	
+    /**
+     * Default constructor. 
+     */
+    public Advise() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    public String getAdvise(){
+    	saveBook();
+    	return "Inserted data in table";
+    }
+    
+    private boolean saveBook() {
+    	boolean flag = false;
+    	Book book = new Book();
+    	book.setId(1);
+    	book.setBookname("EJB 3.0");
+    	em.persist(book);
+    	return flag;
+    }
+
+}
