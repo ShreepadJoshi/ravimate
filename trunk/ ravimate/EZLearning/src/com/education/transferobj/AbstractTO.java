96f684784948ee8b0347279462c6b842d63d7337
shreepadjoshi <shreepadjoshi@35691da0-5893-11de-8832-3f4c773c73ae>
2010-08-23 03:31:07 +0000

    deleted

--- trunk/ ravimate/EZLearning/src/com/education/transferobj/AbstractTO.java
+++
@@ -1,9 +0,0 @@
-package com.education.transferobj;
-
-import java.io.Serializable;
-
-public abstract class AbstractTO implements Serializable{
-	
-	public static final String  NEW_LINE = ""+(char)13;  
-
-}
