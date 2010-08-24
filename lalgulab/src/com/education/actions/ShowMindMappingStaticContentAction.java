6a43c6289d009df5df7ed484629b097eb59f1e0f
patilsmita1011 <patilsmita1011@35691da0-5893-11de-8832-3f4c773c73ae>
2010-08-24 11:13:24 +0000

    Added action classes for flash content display by Smita - 24th aug

---
+++ lalgulab/src/com/education/actions/ShowMindMappingStaticContentAction.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.education.actions;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+
+import com.education.Session.SessionConstants;
+import com.education.Session.UserSessionInfo;
+import com.education.displaytag.IExtendedPaginatedList;
+import com.education.displaytag.QueDescColDecorator;
+import com.education.formbeans.AddQuestionForm;
+import com.education.services.QuestionBankService;
+import com.education.transferobj.QuestionBankTO;
+import com.education.util.EducationConstant;
+import com.education.util.Utilities;
+
+import javax.naming.directory.SearchResult;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
+import org.apache.struts.upload.FormFile;
+import org.expframework.data.ExceptionDisplayDTO;
+
+/**
+ *
+ * @author Administrator
+ */
+public class ShowMindMappingStaticContentAction extends EducationBaseAction{
+
+    @Override
+    public ActionForward displayAction(ActionMapping mapping, ActionForm aForm, HttpServletRequest request, HttpServletResponse response) throws Exception {
+    	return mapping.findForward("mind_mapping");    
+       }
+
+    @Override
+    public ActionForward performAction(ActionMapping mapping, ActionForm aForm, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        AddQuestionForm addQuestionForm = (AddQuestionForm)aForm;
+       String strActionForward =null;
+    	
+        return mapping.findForward(strActionForward);
+    }
+
+	
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
