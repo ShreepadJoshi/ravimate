648d008e1132d64903d021e073b3d8b05aebf874
shreepadjoshi@gmail.com <shreepadjoshi@gmail.com@35691da0-5893-11de-8832-3f4c773c73ae>
2012-04-15 10:34:56 +0000

    UnUsed Persistence code is removed

--- trunk/EJB3.0/EJB3SessionBean/ejbModule/com/Advise.java
+++ trunk/EJB3.0/EJB3SessionBean/ejbModule/com/Advise.java
@@ -7,10 +7,6 @@ import javax.ejb.Stateless;
 import javax.ejb.TimedObject;
 import javax.ejb.Timer;
 import javax.interceptor.Interceptors;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-import pojo.Book;
 
 /**
  * Session Bean implementation class Advise
@@ -18,41 +14,28 @@ import pojo.Book;
 
 @Remote(AdviseRemote.class)
 @Stateless
-@Interceptors( { ShreeInterceptor.class })
+@Interceptors({ ShreeInterceptor.class })
+public class Advise implements TimedObject {
 
-public class Advise implements TimedObject{
+	// @PersistenceContext EntityManager em;
 
-	
-	
-	@PersistenceContext	EntityManager em;
-	
-    /**
-     * Default constructor. 
-     */
-    public Advise() {
-        // TODO Auto-generated constructor stub
-    }
-    
-    @Interceptors( { ShreeInterceptor.class })
-    public String getAdvise(){
-    	saveBook();
-    	return "Inserted data in table";
-    }
-    
-    private boolean saveBook() {
-    	boolean flag = false;
-    	Book book = new Book();
-    	book.setId(1);
-    	book.setBookname("EJB 3.0");
-    	em.persist(book);
-    	return flag;
-    }
+	/**
+	 * Default constructor.
+	 */
+	public Advise() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Interceptors({ ShreeInterceptor.class })
+	public String getAdvise() {
+		// saveBook();
+		return "This is Advise Stateless EJB";
+	}
 
 	@Override
 	public void ejbTimeout(Timer arg0) {
 		System.out.println("Timer Working");
-		
+
 	}
-    
 
 }
