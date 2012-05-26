47627afb6a4295e36c5d4a49543aaab3d6315a0b
shreepadjoshi <shreepadjoshi@gmail.com>
2012-05-26 07:07:28 +0000

    just message is updated

--- EJB3.0/EJB3SessionBean/ejbModule/interceptors/ShreeInterceptor.java
+++ EJB3.0/EJB3SessionBean/ejbModule/interceptors/ShreeInterceptor.java
@@ -6,7 +6,7 @@ public class ShreeInterceptor {
 	
 	@AroundInvoke
 	public Object log(javax.interceptor.InvocationContext invocationContext) throws Exception {
-		System.err.println(invocationContext.getMethod().getName() + " called from ShreeInterceptor");
+		System.err.println(invocationContext.getMethod().getName() + " In ShreeInterceptor");
 		return invocationContext.proceed();
 	}
 }
