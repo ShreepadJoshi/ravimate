cc1393e2f0ac7623c2f41784c41b12a90becad51
shreepadjoshi@gmail.com <shreepadjoshi@gmail.com@35691da0-5893-11de-8832-3f4c773c73ae>
2012-06-12 04:41:43 +0000

    Temp added

---
+++ trunk/ ravimate/srcmate/temp_JPA_src/TestJPA/TestMain.java
@@ -0,0 +1,16 @@
+package TestJPA;
+
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JPACreate create = new JPACreate();
+		
+		create.createPerson();
+		
+
+	}
+
+}
