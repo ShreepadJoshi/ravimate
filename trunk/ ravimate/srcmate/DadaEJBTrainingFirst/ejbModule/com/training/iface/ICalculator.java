2215009a086d738f56f0ffe12819983b748aa516
shreepadjoshi@gmail.com <shreepadjoshi@gmail.com@35691da0-5893-11de-8832-3f4c773c73ae>
2012-06-12 04:45:29 +0000

    removed from here as moved to EJB 3.0 project

--- trunk/ ravimate/srcmate/DadaEJBTrainingFirst/ejbModule/com/training/iface/ICalculator.java
+++
@@ -1,9 +0,0 @@
-package com.training.iface;
-
-import javax.ejb.Remote;
-
-@Remote
-public interface ICalculator {
-	public double add(double i, double j);
-	public double sub(double i, double j);
-}
