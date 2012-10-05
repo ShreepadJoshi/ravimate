f6266b42f9d702d3e1bcfc9d62f20b2fbe2e3d6b
shreepadjoshi@gmail.com <shreepadjoshi@gmail.com@35691da0-5893-11de-8832-3f4c773c73ae>
2012-10-05 02:27:09 +0000

    run net call in new thread ...it was needed for 4.0

--- trunk/MarathiNews/src/com/news/MarathiNewsActivity.java
+++ trunk/MarathiNews/src/com/news/MarathiNewsActivity.java
@@ -2,14 +2,18 @@ package com.news;
 
 import android.Manifest.permission;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.news.adapter.NewsViewDataAdapter;
 
 public class MarathiNewsActivity extends Activity {
+
+	Handler handler = new Handler();
 
 	/** Called when the activity is first created. */
 	@Override
@@ -19,15 +23,42 @@ public class MarathiNewsActivity extends Activity {
 
 		PackageManager pm = getPackageManager();
 		if (pm.checkPermission(permission.INTERNET, getPackageName()) == PackageManager.PERMISSION_GRANTED) {
-		
-			ListView listView = (ListView) findViewById(R.id.newsListView);
-			NewsViewDataAdapter adapter = new NewsViewDataAdapter(getAssets());
-			listView.setAdapter(adapter);
 			
+			loadNewsInNewThread();
+
 		} else {
 			Toast.makeText(MarathiNewsActivity.this,
 					"Please grante the permission for internet",
-					Toast.LENGTH_LONG);
+					Toast.LENGTH_LONG).show();
 		}
+	}
+
+	/**
+	 * @return
+	 */
+	private void loadNewsInNewThread() {
+		final ProgressDialog dialog = ProgressDialog.show(this, "Loading",
+				"loading news");
+
+		Thread t = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				final NewsViewDataAdapter adapter = new NewsViewDataAdapter(
+						getAssets());
+				
+				handler.post(new Runnable() {
+					@Override
+					public void run() {
+						ListView listView = (ListView) findViewById(R.id.newsListView);
+						listView.setAdapter(adapter);
+					}
+				});
+
+				dialog.dismiss();
+			}
+		});
+		t.start();
+
 	}
 }
