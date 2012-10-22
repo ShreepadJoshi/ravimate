c3918bcc0f32d2c0caef84f9fd6ec3c44fac8e07
shreepadjoshi@gmail.com <shreepadjoshi@gmail.com@35691da0-5893-11de-8832-3f4c773c73ae>
2012-10-22 15:47:17 +0000

    code for Lookup is added

--- trunk/EJB3.0/EJB3Test/src/com/EJBTester.java
+++ trunk/EJB3.0/EJB3Test/src/com/EJBTester.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.ejb.EJB;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,6 +27,17 @@ public class EJBTester extends HttpServlet {
 		out.println("In servlet, Calling EJB <Br>");
 		String message4mEJB = remote.getAdvise().toString();
 		out.print("Message 4m EJB is ===>" + message4mEJB);
+		
+		Context ctx;
+		try {
+			ctx = new InitialContext();
+			AdviseRemote remote2 =  (AdviseRemote) ctx.lookup("Advise/remote");
+			out.print("This is remote 2 ===>" + remote2);
+		} catch (NamingException e) {			
+			e.printStackTrace();
+		}
+		
+		
 
 	}
 
