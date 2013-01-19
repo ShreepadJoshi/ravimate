d61a2c8b85e1e35b0ceda879465b6785e066eca1
shreepadjoshi <shreepadjoshi@gmail.com>
2013-01-19 04:49:12 +0000

    method added to increase font size on volume up and down event.

--- MarathiNews/src/com/news/NewsDetailActivity.java
+++ MarathiNews/src/com/news/NewsDetailActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.widget.TextView;
 
 import com.news.bean.NewsBean;
@@ -13,6 +14,7 @@ import com.news.util.MarathiFontUtil;
 public class NewsDetailActivity extends Activity {
 
 	TextView detailView;
+	static int fontsize = 10;// TODO this value must save and reload from DB
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +32,7 @@ public class NewsDetailActivity extends Activity {
 
 		detailView = (TextView) findViewById(R.id.detailPage_detail);
 		detailView.setTypeface(marathiFont);
+		detailView.setTextSize(fontsize);
 		// detailView.setText(news.getDescription());
 
 		readNewsFromWebPage(news);
@@ -45,29 +48,22 @@ public class NewsDetailActivity extends Activity {
 		@Override
 		protected String doInBackground(String... urls) {
 			StringBuilder responseInHTML = new StringBuilder("");
-			
-			
+
 			for (String url : urls) {
-				
+
 				/*
-				DefaultHttpClient client = new DefaultHttpClient();
-				HttpGet httpGet = new HttpGet(url);
-				try {
-					HttpResponse execute = client.execute(httpGet);
-					InputStream content = execute.getEntity().getContent();
+				 * DefaultHttpClient client = new DefaultHttpClient(); HttpGet
+				 * httpGet = new HttpGet(url); try { HttpResponse execute =
+				 * client.execute(httpGet); InputStream content =
+				 * execute.getEntity().getContent();
+				 * 
+				 * BufferedReader buffer = new BufferedReader( new
+				 * InputStreamReader(content)); String s = ""; while ((s =
+				 * buffer.readLine()) != null) { responseInHTML.append(s); }
+				 * 
+				 * } catch (Exception e) { e.printStackTrace(); }
+				 */
 
-					BufferedReader buffer = new BufferedReader(
-							new InputStreamReader(content));					
-					String s = "";
-					while ((s = buffer.readLine()) != null) {
-						responseInHTML.append(s);
-					}
-
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				*/
-				
 				responseInHTML = ReadESakal.readHTMLPageOfNews(url);
 			}
 			return ReadESakal.extractNewsFromHTML(responseInHTML);
@@ -77,6 +73,29 @@ public class NewsDetailActivity extends Activity {
 		protected void onPostExecute(String result) {
 			detailView.setText(result);
 		}
+	}
+
+	/**
+	 * Key down even listener
+	 * 
+	 * we will use this to get Volume UP and DOWN action, method will increase
+	 * font size on volume up and down event.
+	 */
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		super.onKeyDown(keyCode, event);
+
+		if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
+			System.out.println("key presss KEYCODE_VOLUME_UP");
+			fontsize = fontsize + 2;
+			return true;
+		}
+		if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+			fontsize = fontsize - 2;
+			System.out.println("key presss KEYCODE_VOLUME_DOWN");
+			return true;
+		}
+		detailView.setTextSize(fontsize);
+		return true;
 	}
 
 }
