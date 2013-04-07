740d9c4968795a080ce2ff9d75954510df2e8cc4
shreepadjoshi@gmail.com <shreepadjoshi@gmail.com@35691da0-5893-11de-8832-3f4c773c73ae>
2013-04-07 07:11:32 +0000

    UILogger added

---
+++ trunk/BreakableToys/vlcPlayerDictionaryPlagin/src/com/log/UILogger.java
@@ -0,0 +1,16 @@
+package com.log;
+
+import com.ui.panel.LogPlanel;
+
+public class UILogger {
+
+	public static void log(String log) {
+		LogPlanel.getInstance().displayLog(log);
+		System.out.println(log);
+	}
+	
+	public static void log(Object log) {
+		log(log.toString());
+	}
+
+}
