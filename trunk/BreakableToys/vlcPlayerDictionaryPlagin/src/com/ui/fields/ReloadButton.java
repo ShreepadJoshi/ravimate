ee91c97007e802cce180f4f119837bb7200c2398
shreepadjoshi@gmail.com <shreepadjoshi@gmail.com@35691da0-5893-11de-8832-3f4c773c73ae>
2013-04-07 09:02:32 +0000

    package changed

---
+++ trunk/BreakableToys/vlcPlayerDictionaryPlagin/src/com/ui/fields/ReloadButton.java
@@ -0,0 +1,16 @@
+package com.ui.fields;
+
+import javax.swing.JButton;
+
+public class ReloadButton extends JButton {
+
+	public ReloadButton() {
+		this.setText("Reload");
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
