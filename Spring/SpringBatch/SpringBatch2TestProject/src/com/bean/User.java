50df0542140368a05334b94e16651374ffe8e55f
shreepadjoshi <shreepadjoshi@gmail.com>
2013-04-21 03:58:34 +0000

    making class as (JPA) Entity, for JPA example.

--- Spring/SpringBatch/SpringBatch2TestProject/src/com/bean/User.java
+++ Spring/SpringBatch/SpringBatch2TestProject/src/com/bean/User.java
@@ -1,14 +1,22 @@
 package com.bean;
 
+import static com.util.Constants.NEW_LINE;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import static com.util.Constants.NEW_LINE;;
-
 @XmlRootElement(name = "User")
 @XmlAccessorType(XmlAccessType.FIELD)
+
+@Entity
+@Table(name = "t_user")
 public class User {
 	
 	@XmlElement
@@ -27,6 +35,9 @@ public class User {
 	}
 	
 	
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
 	public String getId() {
 		return id;
 	}
@@ -34,12 +45,15 @@ public class User {
 		this.id = id;
 	}
 	
+	@Column(name = "first_name")
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	@Column(name = "last_name")
 	public String getLastName() {
 		return lastName;
 	}
