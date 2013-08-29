a9820fe91ed488e9af2f282a827b98ba9e897947
shreepadjoshi <shreepadjoshi@gmail.com>
2013-08-29 01:50:16 +0000

    added here

---
+++ z_other/ ravimate/srcmate/src/com/Temp.java
@@ -0,0 +1,23 @@
+package com;
+
+public class Temp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// customers.toArray(new Customer[0]);
+		
+		//dagaDu.toArray(new Customer[0]);
+		
+		//mabadu.toArray(new Customer[0]);
+
+		// customers.toArray(new Customer[customers.size()]);
+
+		// private static List<Customer> customers ;
+
+	}
+
+}
