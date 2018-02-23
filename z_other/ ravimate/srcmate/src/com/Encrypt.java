df717c3e9540d03a1022decd6c8d66921424d814
ShreepadJoshi <ShreepadJoshi@users.noreply.github.com>
2018-02-23 14:55:01 +0530

    added as a backup

---
+++ z_other/ ravimate/srcmate/src/com/Encrypt.java
@@ -0,0 +1,40 @@
+package com;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.swing.JOptionPane;
+
+public class Encrypt {
+
+	public static void main(String[] args) {
+		/*
+		 * System.out.println("Enter the password : "); Scanner reader = new
+		 * Scanner(System.in); String password = reader.next();
+		 */
+
+		String password = JOptionPane.showInputDialog("Please enter password: ");
+		writeEncryptedDateToFile(password);
+
+	}
+
+	public static void writeEncryptedDateToFile(String password) {
+		AES en = new AES(password);
+
+		File input = new File("input.txt");
+		File eoutput = new File("encrypted.txt");
+
+		try {
+			en.WriteEncryptedFile(new FileInputStream(input), new FileOutputStream(eoutput));
+
+		} catch (IllegalBlockSizeException | BadPaddingException | IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
